08cf0643d5115412df26ac77e116605e9ccbeef5
Song Gao <song.gao.beta@gmail.com>
2012-11-08 15:59:11 -0500

    -

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+import tornado.ioloop
+import tornado.web
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Hello, world")
+
+application = tornado.web.Application([
+    (r"/", MainHandler),
+])
+
+if __name__ == "__main__":
+    application.listen(os.environ["PORT"], os.environ["IP"])
+    tornado.ioloop.IOLoop.instance().start()
